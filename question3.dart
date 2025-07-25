999b9ef4f0508243d3caf38a132536afe26c8757
abdulraffay14 <abdulraffay9292@gmail.com>
2025-07-25 23:19:58 +0500

    assignment 4 question 3

---
+++ question3.dart
@@ -0,0 +1,9 @@
+void main() {
+  var student = ['Muhammad Raffay', '15th Grade', 22, 'A+', 89.5];
+
+  print('Name: ${student[0]}');
+  print('Class: ${student[1]}');
+  print('Roll No: ${student[2]}');
+  print('Grade: ${student[3]}');
+  print('Percentage: ${student[4]}%');
+}
